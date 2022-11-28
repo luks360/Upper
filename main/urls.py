d35aef0b3ff28ef7254dfd73d13a39687a4ac277
Lucas dos Santos <santoslucasantos796@gmail.com>
2022-11-28 18:01:37 -0300

    Criação de metodo de logout

--- main/urls.py
+++ main/urls.py
@@ -1,9 +1,10 @@
 from django.urls import path
-from .views import home, dashboard, signIn, signUp
+from .views import home, dashboard, signIn, signUp, exit
 
 urlpatterns = [
     path('', home, name='home'),
     path('dashboard', dashboard, name='dashboard'),
     path('signin', signIn, name='signin'),
     path('signup', signUp, name='signup'),
+    path('logout', exit, name='logout')
 ]
